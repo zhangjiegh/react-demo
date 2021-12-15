7c5be082090a19e4f7795c6e32f6a20c1568125e
zhangjie <13144126585@163.com>
2021-12-15 21:12:53 +0800

    feat:添加 prettier 统一格式化代码

---
+++ .prettierrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  tabWidth: 2, // 缩进字节数
+  semi: true, // 句尾添加分号
+  trailingComma: "all", // 在对象或数组最后一个元素后面是否加逗号
+  arrowParens: "avoid", //  (x) => {} 箭头函数参数只有一个时是否要有小括号。avoid：省略括号
+};
